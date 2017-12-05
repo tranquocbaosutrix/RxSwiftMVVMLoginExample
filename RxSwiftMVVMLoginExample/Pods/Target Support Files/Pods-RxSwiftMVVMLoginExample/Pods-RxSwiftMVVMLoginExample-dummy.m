5dc3b5c613ecf25533590df8fc1a9167f39efabb
Tran Quoc Bao <bao.tq@sutrixsolutions.com>
2017-12-05 14:35:49 +0700

    [Edited] Create a new iOS Project with name 'RxSwiftMVVMLoginExample'. This project is using RxSwift and MVVM Pattern.

---
+++ RxSwiftMVVMLoginExample/Pods/Target Support Files/Pods-RxSwiftMVVMLoginExample/Pods-RxSwiftMVVMLoginExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftMVVMLoginExample : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftMVVMLoginExample
+@end
---
+++ RxSwiftMVVMLoginExample/Pods/Target Support Files/Pods-RxSwiftMVVMLoginExampleUITests/Pods-RxSwiftMVVMLoginExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftMVVMLoginExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftMVVMLoginExampleUITests
+@end
